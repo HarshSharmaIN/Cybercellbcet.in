fc730877b651d84f86be8994f9855210083eb9d9
27Adityasahil <adityasahil24@gmail.com>
2024-11-28 16:38:47 +0530

    Form and team section is designed

---
+++ src/components/Team.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './Team.css';
+
+const Team = () =>{
+    return <section>
+        Hello World!
+        <div className="Team-container gradient">
+            <div className="team-left-section">
+                <h3>Our Team</h3>
+                <div className="team-span">
+                    <span className="position">Founding Members</span>
+                    <span className="position">Teacher Coordinators</span>
+                    <span className="position">Present Team</span>
+                </div>
+            </div>
+            <div className="team-right-section">
+                
+            </div>
+        </div>
+    </section>
+}
+export default Team;
